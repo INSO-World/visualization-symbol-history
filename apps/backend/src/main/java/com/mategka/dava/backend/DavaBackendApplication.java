266ba7287959241322bb428ad09bfccf414112b6
André Mategka <12amdeveloper@gmail.com>
2024-08-01 20:45:27 +0200

    Add Nx monorepo stub

---
+++ apps/backend/src/main/java/com/mategka/dava/backend/DavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mategka.dava.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavaBackendApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/com/mategka/dava/backend/DavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mategka.dava.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

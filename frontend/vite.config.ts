4d56dc1f8c674b9b78057eb93ce09593fe07165d
André Mategka <12amdeveloper@gmail.com>
2024-11-25 21:38:20 +0100

    Enable environments for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,37 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { configDotenv } from 'dotenv'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const envPath = new URL(`environments/${mode}.env`, import.meta.url)
+  const env = configDotenv({ path: envPath }).parsed ?? {
+    VITE_HOST: 'localhost',
+    VITE_PORT: '5173',
+  }
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+    server: {
+      host: env.VITE_HOST,
+      port: Number.parseInt(env.VITE_PORT, 10),
+    },
+    define: {
+      'process.env': {
+        ...process.env,
+        ...env,
+      },
+    },
+  } satisfies UserConfig
 })

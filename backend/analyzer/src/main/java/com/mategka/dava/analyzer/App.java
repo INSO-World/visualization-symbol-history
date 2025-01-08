04e4648e03ade177cd82f18ff00277fc788a45f4
André Mategka <12amdeveloper@gmail.com>
2025-01-08 18:26:03 +0100

    Update analyzer test code

--- backend/analyzer/src/main/java/com/mategka/dava/analyzer/App.java
+++ backend/analyzer/src/main/java/com/mategka/dava/analyzer/App.java
@@ -1,12 +1,22 @@
 package com.mategka.dava.analyzer;
 
+import com.mategka.dava.analyzer.git.Commits;
 import com.mategka.dava.analyzer.git.Repositories;
-import com.mategka.dava.analyzer.struct.History;
+import com.mategka.dava.analyzer.util.Optionals;
+import org.eclipse.jgit.diff.DiffAlgorithm;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
-import spoon.MavenLauncher;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.util.io.NullOutputStream;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class App {
 
@@ -14,7 +24,42 @@ public class App {
     System.out.println("Hello World!");
     try (Repository repository = Repositories.open("?REPO")) {
       Ref mainBranch = repository.findRef("main");
-      var history = History.emptyOfBranch(repository, mainBranch);
+      //var history = History.emptyOfBranch(repository, mainBranch);
+      var testChangeTypes = List.of(DiffEntry.ChangeType.ADD, DiffEntry.ChangeType.MODIFY);
+      var timeBefore = System.currentTimeMillis();
+      int offset = 0;
+      try (RevWalk walk = Commits.topologicalReverse(repository, mainBranch)) {
+        for (RevCommit commit : walk) {
+          System.out.print(commit.getId().getName().substring(0, 6) + " ");
+          if (++offset >= 12) {
+            offset = 0;
+            System.out.println();
+          }
+          var parent = Optionals.getFirst(commit.getParents());
+          try (DiffFormatter formatter = new DiffFormatter(NullOutputStream.INSTANCE)) {
+            formatter.setRepository(repository);
+            formatter.setContext(0);
+            formatter.setDetectRenames(true);
+            formatter.setDiffAlgorithm(DiffAlgorithm.getAlgorithm(DiffAlgorithm.SupportedAlgorithm.MYERS));
+            var renameDetector = formatter.getRenameDetector();
+            renameDetector.setRenameScore(50);
+            var diffs = formatter.scan(parent.map(RevCommit::getTree).orElse(null), commit.getTree());
+            for (var diff : diffs) {
+              var path = diff.getNewPath();
+              if (testChangeTypes.contains(diff.getChangeType())) {
+                var objectId = diff.getNewId().toObjectId();
+                try (ObjectReader reader = repository.newObjectReader(); ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+                  reader.open(objectId).copyTo(output);
+                  var content = output.toString(StandardCharsets.UTF_8);
+                }
+              }
+            }
+          }
+        }
+      }
+      System.out.println("Done!");
+      var time = System.currentTimeMillis() - timeBefore;
+      System.out.println("Time (ms): " + time);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
--- backend/analyzer/src/main/java/com/mategka/dava/analyzer/util/Optionals.java
+++ backend/analyzer/src/main/java/com/mategka/dava/analyzer/util/Optionals.java
@@ -2,14 +2,37 @@ package com.mategka.dava.analyzer.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.Collection;
 import java.util.Optional;
+import java.util.SequencedCollection;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 @UtilityClass
 public class Optionals {
 
+
+
   public <T> Optional<T> cast(Optional<? super T> optional, Class<T> targetType) {
     return optional.filter(targetType::isInstance).map(targetType::cast);
+  }
+
+  public <T> Optional<T> when(boolean condition, Supplier<T> supplier) {
+    return condition ? Optional.of(supplier.get()) : Optional.empty();
+  }
+
+  public <T> Optional<T> getFirst(SequencedCollection<? extends T> collection) {
+    return when(!collection.isEmpty(), collection::getFirst);
+  }
+
+  public <T> Optional<T> getFirst(Iterable<? extends T> iterable) {
+    var iterator = iterable.iterator();
+    return when(iterator.hasNext(), iterator::next);
+  }
+
+  public <T> Optional<T> getFirst(T[] array) {
+    return when(array.length > 0, () -> array[0]);
   }
 
 }
